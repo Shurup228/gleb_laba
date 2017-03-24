7a06f4c5e422c31c6f9f236447bf8bf2d0734fd7
Shurup228 <yroslav541@gmail.com>
2017-03-25 00:30:54 +0200

    CSS ready, started to work on js.

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 'use strict'
 
-PILE_AMOUNT = 5;
-ROCK_AMOUNT = 42;
-TURN_AMOUNT = [1, 2, 3];
+var PILE_AMOUNT = 5;
+var ROCK_AMOUNT = 42;
+var TURN_AMOUNT = [1, 2, 3];
+
+const pilesDiv = document.getElementById('piles');
+
+for (let i = 0; i < PILE_AMOUNT; i++) {
+  let pile = document.createElement('div');
+  pile.innerHTML = '<p>I\'m a pile';
+  pilesDiv.appendChild(pile);
+}
