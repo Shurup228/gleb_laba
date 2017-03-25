3d662e122cf445e7bc579def946571188c955e54
Shurup228 <yroslav541@gmail.com>
2017-03-25 20:31:58 +0200

    minor bug fixes

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
 'use strict'
 
-var PILE_AMOUNT = 5;
-var ROCK_AMOUNT = 42;
-var TURN_AMOUNT = [1, 2, 3];
+const PILE_AMOUNT = 5;
+const ROCK_AMOUNT = 42;
+const TURN_AMOUNT = [1, 2, 3];
+const pilesDiv = document.getElementById('piles');
+const turnsDiv = document.getElementById('turns');
 
 {
-  const pilesDiv = document.getElementById('piles');
-  const turnsDiv = document.getElementById('turns');
-
   for (let i = 0; i < PILE_AMOUNT; i++) {
     let pile = document.createElement('div');
     let text = document.createElement('p');
 
     text.innerHTML = `Pile #${i + 1}`;
     pile.dataset.rocks = ROCK_AMOUNT;
+    pile.dataset.num = i + 1;
 
     pile.appendChild(text);
     pilesDiv.appendChild(pile);
   }
 
-  TURN_AMOUNT.forEach((elem) => {
+  TURN_AMOUNT.forEach((elem, index) => {
     let turn = document.createElement('div');
     let text = document.createElement('p');
 
     turn.dataset.turn = elem;
+    turn.dataset.num = index + 1;
     text.innerHTML = 'I\'m a turn';
 
     turn.appendChild(text);
@@ -43,16 +44,18 @@ var TURN_AMOUNT = [1, 2, 3];
     elem.style.transform = 'scale(1.05, 1.05)';
   }
 
-  function action(elem, divs) {
+  function action(elem, divs, parent) {
     elem.addEventListener('click', function (event) {
       divs.forEach((el) => {
 
-        if (el.style.transform === 'scale(1.05, 1.05)') {
-          hide(el);
-          return;
-        }
-
         if (el === elem) {
+          if (el.style.transform === 'scale(1.05, 1.05)') {
+            parent.dataset.chosen = '';
+            hide(el);
+            return;
+          }
+
+          parent.dataset.chosen = el.dataset.num;
           show(el);
         } else {
           hide(el);
@@ -62,10 +65,10 @@ var TURN_AMOUNT = [1, 2, 3];
   }
 
   piles.forEach((elem) => {
-    action(elem, piles);
+    action(elem, piles, pilesDiv);
   });
 
   turns.forEach((elem) => {
-    action(elem, turns);
+    action(elem, turns, turnsDiv);
   });
 }
