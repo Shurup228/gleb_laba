5f4b4d77835df263c86b1ed29c1a3dd903c02868
Shurup228 <yroslav541@gmail.com>
2017-03-25 18:28:54 +0200

    made floating card like effect and did some code organisation

--- script.js
+++ script.js
@@ -5,9 +5,26 @@ var ROCK_AMOUNT = 42;
 var TURN_AMOUNT = [1, 2, 3];
 
 const pilesDiv = document.getElementById('piles');
+const turnsDiv = document.getElementById('turns');
 
 for (let i = 0; i < PILE_AMOUNT; i++) {
   let pile = document.createElement('div');
-  pile.innerHTML = '<p>I\'m a pile';
+  let text = document.createElement('p');
+
+  text.innerHTML = `Pile #${i + 1}`;
+  pile.dataset.rocks = ROCK_AMOUNT;
+
+  pile.appendChild(text);
   pilesDiv.appendChild(pile);
 }
+
+TURN_AMOUNT.forEach((elem, index, arr) => {
+  let turn = document.createElement('div');
+  let text = document.createElement('p');
+
+  turn.dataset.turn = elem;
+  text.innerHTML = 'I\'m a turn';
+
+  turn.appendChild(text);
+  turnsDiv.appendChild(turn);
+});
